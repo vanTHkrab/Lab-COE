59781d0332243b4f6b2c36070c070af7161b9d4b
vanTHkrab <vanth2548@gmail.com>
2023-10-05 22:11:01 +0700

    111

--- homework/Homework1.c
+++ homework/Homework1.c
@@ -2,25 +2,24 @@
 # include <math.h>
 
 int main(void) {
-    float oil, WRate;
+    float oil = 0.00f, WRate = 0.00f;
     printf("Enter the oil capacity (liter): ");
     scanf("%f", &oil);
     printf("Enter the oil consumption rate (km/liter): ");
     scanf("%f", &WRate);
-    float distance = abs((float)oil * WRate);
+    float distance = abs(oil * WRate);
 
     printf("The distance is %.2f km\n", distance);
     if (distance < 100) {
         printf("City A");
-    } 
+    }
     else if (distance >= 100 && distance < 250) {
         printf("City B");
-    } 
+    }
     else if (distance >= 250 && distance < 500) {
         printf("City C");
     }
     else if (distance >= 500) {
         printf("City D");
     }
-    return 0;
 }
