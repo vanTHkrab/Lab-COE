02fa9ae7c0c3626f8d6abf8c87d11377804e2fb2
vanTHkrab <vanth2548@gmail.com>
2023-10-10 22:06:27 +0700

    Fixed lab version 1.12

--- present-lab/Lab2.c
+++ present-lab/Lab2.c
@@ -5,7 +5,7 @@ int main(void) {
     int num;
     printf("Input the binary number: ");
     scanf("%d", &num);
-    num = abs(num);
+    int binary = abs(num);
     int i = 0, sum = 0, t = 1;
     for (int i = num ; i > 0; i /= 10) {
         if (num % 10 == 1) {
@@ -21,5 +21,5 @@ int main(void) {
         num /= 10;
         t *= 2;
     }
-    printf("The equivalent Decimal Number is : %d\n", sum);
+    printf("This Binary Code \"%d\" And The equivalent Decimal Number is : %d\n", binary, sum);
 }
