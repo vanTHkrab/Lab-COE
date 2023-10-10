ff6742489b8ae0ff172095ca5679ba38ca0aeccb
vanTHkrab <vanth2548@gmail.com>
2023-10-10 21:54:58 +0700

    Update assingment1-1.c

--- assingment/assingment1-1.c
+++ assingment/assingment1-1.c
@@ -1,11 +1,11 @@
 # include <stdio.h>
+# include <math.h>
 
 int main(void){
-    float b, h, area;
+    float b, h;
     printf("Enter the base: ");
     scanf("%f", &b);
     printf("Enter the height: ");
     scanf("%f", &h);
-    area = b * h / 2;
-    printf("The Triangle Area is %.2f\n", area);
+    printf("The Triangle Area is %.2f\n", abs(b * h / 2));
 }
