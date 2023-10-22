c643f22a96666067cbc9b65ea4671777c0d970fd
vanTHkrab <vanth2548@gmail.com>
2023-10-23 00:29:10 +0700

    ....

--- homework/Homework2.c
+++ homework/Homework2.c
@@ -38,6 +38,10 @@ int main(void) {
             d[z] = a[i];
         }
     }
+    if (z == 0){
+        printf("All number are same\n");
+        return 0;
+    }
 //-----------------------------------------------------------------------
 // count number of each number
     int total = 0, time[z];
