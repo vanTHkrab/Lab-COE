46733b73a5a34828b11a008cb4b65a8ea834b14b
vanTHkrab <vanth2548@gmail.com>
2023-12-05 12:46:24 +0700

    Bank c

--- workspace_van/homework/Homework5_Test/index.c
+++ workspace_van/homework/Homework5_Test/index.c
@@ -3,202 +3,119 @@
 #include <stdlib.h>
 
 typedef struct datacredit *idcredit;
-
 struct datacredit {
     char id[10];
     char creditid[10];
     char type[15], pin[10];
-    int num;
     float limit;
     float balance;
+    idcredit creditnext;
 };
 
 typedef struct datacustomer *idcustomer;
-
 struct datacustomer {
     char id[10];
     char name[30];
     char lastname[30];
+    int num;
     struct datacredit *credit;
+    idcustomer next;
 };
 
-void createcustomer(idcustomer customer) {
-    printf("Enter Customer ID: ");
-    scanf("%s", customer->id);
-    printf("Enter Customer name: ");
-    scanf("%s", customer->name);
-    printf("Enter Customer lastname: ");
-    scanf("%s", customer->lastname);
-}
-
-void createcredit(idcustomer customer) {
-    int type;
-    char creditid[10], id[10];
-
+idcustomer createcustomer(idcustomer customer) {
+    idcustomer newnode;
+    idcustomer temp = customer;
+    char id[10], name[30], lastname[30];
     printf("Enter Customer ID: ");
     scanf("%s", id);
-
-    while (strcmp(id, customer->id) != 0) {
-        printf("Customer ID not found. Please try again.\n");
-    }
-
-    if (customer->credit == NULL) {
-        customer->credit = (idcredit)malloc(sizeof(struct datacredit));
-        customer->credit->num = 0;
-    }
-
-    idcredit credit = customer->credit;
-    printf("Enter Credit ID: ");
-    scanf("%s", creditid);
-
-    for (int i = 0; i < credit->num; i++) {
-        if (strcmp(creditid, credit[i].creditid) == 0) {
-            printf("Credit ID already exists. Please try again.\n");
-            return;
+    while (temp != NULL) {
+        if (strcmp(temp->id, id) == 0) {
+            printf("ID already exists\n");
+            return customer;
         }
+        temp = temp->next;
     }
+    printf("Enter Customer name: ");
+    scanf("%s", name);
+    printf("Enter Customer lastname: ");
+    scanf("%s", lastname);
+    newnode = (idcustomer)malloc(sizeof(struct datacustomer));
+    strcpy(newnode->id, id);
+    strcpy(newnode->name, name);
+    strcpy(newnode->lastname, lastname);
+    newnode->num = 0;
+    newnode->credit = NULL;
+    newnode->next = customer;
+    customer = newnode;
+    return customer;
+}
 
-    credit->num++;
-
-    if (credit->num > 10) {
-        printf("Cannot create more than 10 accounts.\n");
-        return;
+idcredit createcredit(idcustomer customer, idcredit credit) {
+    char id[10], type[15], pin[10], cid[10];
+    idcredit newnode = credit;
+    idcustomer temp = customer;
+    if (temp->id == NULL) {
+        printf("No customer\n");
+        return credit;
     }
-    strcpy(credit[credit->num - 1].id, customer->id);
-    strcpy(credit[credit->num - 1].creditid, creditid);
-
-    do {
-        printf("Enter Account type ('1' Savings, '2' Daily Current): ");
-        scanf("%d", &type);
-    } while (type != 1 && type != 2);
-
-    if (type == 1) {
-        printf("Enter Credit balance: ");
-        scanf("%f", &credit[credit->num - 1].balance);
-        credit[credit->num - 1].limit = 0;
-        strcpy(credit[credit->num - 1].type, "Savings");
-        printf("Credit limit: -\n");
-        printf("Enter PIN: ");
-        scanf("%s", credit[credit->num - 1].pin);
+    printf("Enter Customer ID: ");
+    scanf("%s", id);
+    while (strcpy(temp->id, id) != 0) {
+        if (temp->next == NULL) {
+            printf("No customer\n");
+            return credit;
+        }
+        temp = temp->next;
     }
-
-    if (type == 2) {
-        printf("Enter Credit limit: ");
-        scanf("%f", &credit[credit->num - 1].limit);
-        printf("Enter Credit balance: ");
-        scanf("%f", &credit[credit->num - 1].balance);
-        strcpy(credit[credit->num - 1].type, "Daily Current");
-        printf("Enter PIN: ");
-        scanf("%s", credit[credit->num - 1].pin);
+    while (temp != NULL) {
+        if (strcmp(temp->id, id) == 0) {
+            printf("Enter Credit ID: ");
+            scanf("%s", newnode->creditid);
+            if (temp->num >= 10) {
+                printf("Cannot create more than 10 accounts\n");
+                return credit;
+            }
+            newnode = (idcredit)malloc(sizeof(struct datacredit));
+        }
+        temp = temp->next;
     }
 }
+
 
 void money(idcustomer customer) {
-    float money;
-    int t;
-    char creditid[10], id[11];
-    if (customer->credit == NULL) {
-        printf("No credit information available. Please create credit first.\n");
-        return;
-    }
-    printf("Enter Customer ID: ");
-    scanf("%s", id);
-    while (strcmp(id, customer->id) != 0) {
-        printf("Customer ID not found. Please try again.\n");
-    }
-    idcredit credit = customer->credit;
-    printf("Enter Credit ID: ");
-    scanf("%s", creditid);
-    printf("Enter PIN: ");
-    scanf("%s", id);
-    for (int i = 0; i < credit->num; i++) {
-        if (strcmp(creditid, credit[i].creditid) == 0) {
-            if (strcmp(id, credit[i].pin) == 0)
-                break;
-            else {
-                printf("PIN incorrect. Please try again.\n");
-                return;
-            }
-        }
-    }
-    int i;
-    for (i = 0; i < credit->num; i++) {
-        if (strcmp(creditid, credit[i].creditid) == 0)
-            break;
-    }
 
-    if (i == credit->num) {
-        printf("Credit ID not found. Please try again.\n");
-        return;
-    }
-    do{ 
-    printf("1. Deposit\n2. Withdraw\n3. Exit\n->");
-    scanf("%d", &t);
-    if (t == 3) return;
-    }while(t != 1 && t != 2);
-    if (t == 1) {
-        printf("Enter money: ");
-        scanf("%f", &money);
-        credit[i].balance += money;
-        printf("Balance: %.2f\n", credit[i].balance);
-    }
-    else if (t == 2) {
-        printf("Enter money: ");
-        scanf("%f", &money);
-        credit[i].balance -= money;
-        printf("Balance: %.2f\n", credit[i].balance);
-    }
 }
 
 void display(idcustomer customer)
 {
-    int i;
-    if (customer->credit == NULL) {
-        printf("No credit information available. Please create credit first.\n");
-        return;
+
+}
+
+void testdisplay(idcustomer customer)
+{
+    idcustomer temp = customer;
+    while (temp != NULL) {
+        printf("%s %s %s\n", temp->id, temp->name, temp->lastname);
+        temp = temp->next;
     }
-    printf("Customer ID: %s\n", customer->id);
-    printf("Customer name: %s\n", customer->name);
-    printf("Customer lastname: %s\n", customer->lastname);
-    idcredit credit = customer->credit;
-    for (i = 0; i < credit->num; i++) {
-        printf("Credit ID: %s\n", credit[i].creditid);
-        printf("Credit type: %s\n", credit[i].type);
-        printf("Credit balance: %.2f\n", credit[i].balance);
-        printf("Credit limit: %.2f\n", credit[i].limit);
-        printf("Credit PIN: %s\n", credit[i].pin);
-        if (i < credit->num - 1) {
-            printf("\n");
-        }
-    }
-    printf("-------------------------------\n");
 }
 
 int main() {
-    idcustomer customer;
-    customer = (idcustomer)malloc(sizeof(struct datacustomer));
-    customer->credit = NULL;
+    idcustomer customer = NULL;
+    idcredit credit = NULL;
     int k;
     while (1) {
         printf("1. Create Customer\n2. Create Credit (not more than 10 accounts)\n3. Deposit/Withdraw\n4. Display\n5. Exit\n-> ");
         scanf("%d", &k);
         switch (k) {
+        case 0:
+            testdisplay(customer);
+            break;
         case 1:
-            createcustomer(customer);
+            customer = createcustomer(customer);
             break;
         case 2:
-            createcredit(customer);
-            break;
-        case 3:
-            money(customer);
-            break;
-        case 4:
-            display(customer);
-            break;
-        case 5:
-            free(customer->credit);
-            free(customer);
-            exit(0);
+            credit = createcredit(customer, credit);
             break;
         }
         printf("\n");
