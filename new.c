93590f4687fa3ff2d9c0fe49753ac43cbed1ec84
vanTHkrab <vanth2548@gmail.com>
2024-04-19 11:41:02 +0700

    case

---
+++ new.c
@@ -0,0 +1,374 @@
+# include <stdio.h>
+
+int main() {
+    int ans;
+    while(1) {
+        printf("Enter 0 to exit\n");
+        printf("search element->: ");
+        scanf("%d", &ans);
+        switch (ans)
+        {
+        case 0:
+            return 0;
+            break;
+        case 1:
+            printf("H Hydrogen 1.008");
+            break;
+        case 2:
+            printf("He Helium 4.0026");
+            break;
+        case 3:
+            printf("Li Lithium 6.94");
+            break;
+        case 4:
+            printf("Be Beryllium 9.0122");
+            break;
+        case 5:
+            printf("B Boron 10.81");
+            break;
+        case 6:
+            printf("C Carbon 12.011");
+            break;
+        case 7:
+            printf("N Nitrogen 14.007");
+            break;
+        case 8:
+            printf("O Oxygen 15.999");
+            break;
+        case 9:
+            printf("F Fluorine 18.998");
+            break;
+        case 10:
+            printf("Ne Neon 20.180");
+            break;
+        case 11:
+            printf("Na Sodium 22.990");
+            break;
+        case 12:
+            printf("Mg Magnesium 24.305");
+            break;
+        case 13:
+            printf("Al Aluminum 26.982");
+            break;
+        case 14:
+            printf("Si Silicon 28.085");
+            break;
+        case 15:
+            printf("P Phosphorus 30.974");
+            break;
+        case 16:
+            printf("S Sulfur 32.06");
+            break;
+        case 17:
+            printf("Cl Chlorine 35.45");
+            break;
+        case 18:
+            printf("Ar Argon 39.948");
+            break;
+        case 19:
+            printf("K Potassium 39.098");
+            break;
+        case 20:
+            printf("Ca Calcium 40.078");
+            break;
+        case 21:
+            printf("Sc Scandium 44.956");
+            break;
+        case 22:
+            printf("Ti Titanium 47.867");
+            break;
+        case 23:
+            printf("V Vanadium 50.942");
+            break;
+        case 24:
+            printf("Cr Chromium 51.996");
+            break;
+        case 25:
+            printf("Mn Manganese 54.938");
+            break;
+        case 26:
+            printf("Fe Iron 55.845");
+            break;
+        case 27:
+            printf("Co Cobalt 58.933");
+            break;
+        case 28:
+            printf("Ni Nickel 58.693");
+            break;
+        case 29:
+            printf("Cu Copper 63.546");
+            break;
+        case 30:
+            printf("Zn Zinc 65.38");
+            break;
+        case 31:
+            printf("Ga Gallium 69.723");
+            break;
+        case 32:
+            printf("Ge Germanium 72.630");
+            break;
+        case 33:
+            printf("As Arsenic 74.922");
+            break;
+        case 34:
+            printf("Se Selenium 78.971");
+            break;
+        case 35:
+            printf("Br Bromine 79.904");
+            break;
+        case 36:
+            printf("Kr Krypton 83.798");
+            break;
+        case 37:
+            printf("Rb Rubidium 85.468");
+            break;
+        case 38:
+            printf("Sr Strontium 87.62");
+            break;
+        case 39:
+            printf("Y Yttrium 88.906");
+            break;
+        case 40:
+            printf("Zr Zirconium 91.224");
+            break;
+        case 41:
+            printf("Nb Niobium 92.906");
+            break;
+        case 42:
+            printf("Mo Molybdenum 95.95");
+            break;
+        case 43:
+            printf("Tc Technetium 98");
+            break;
+        case 44:
+            printf("Ru Ruthenium 101.07");
+            break;
+        case 45:
+            printf("Rh Rhodium 102.91");
+            break;
+        case 46:
+            printf("Pd Palladium 106.42");
+            break;
+        case 47:
+            printf("Ag Silver 107.87");
+            break;
+        case 48:
+            printf("Cd Cadmium 112.41");
+            break;
+        case 49:
+            printf("In Indium 114.82");
+            break;
+        case 50:
+            printf("Sn Tin 118.71");
+            break;
+        case 51:
+            printf("Sb Antimony 121.76");
+            break;
+        case 52:
+            printf("Te Tellurium 127.60");
+            break;
+        case 53:
+            printf("I Iodine 126.90");
+            break;
+        case 54:
+            printf("Xe Xenon 131.29");
+            break;
+        case 55:
+            printf("Cs Cesium 132.91");
+            break;
+        case 56:
+            printf("Ba Barium 137.33");
+            break;
+        case 57:
+            printf("La Lanthanum 138.91");
+            break;
+        case 58:
+            printf("Ce Cerium 140.12");
+            break;
+        case 59:
+            printf("Pr Praseodymium 140.91");
+            break;
+        case 60:
+            printf("Nd Neodymium 144.24");
+            break;
+        case 61:
+            printf("Pm Promethium 145");
+            break;
+        case 62:
+            printf("Sm Samarium 150.36");
+            break;
+        case 63:
+            printf("Eu Europium 151.96");
+            break;
+        case 64:
+            printf("Gd Gadolinium 157.25");
+            break;
+        case 65:
+            printf("Tb Terbium 158.93");
+            break;
+        case 66:
+            printf("Dy Dysprosium 162.50");
+            break;
+        case 67:
+            printf("Ho Holmium 164.93");
+            break;
+        case 68:
+            printf("Er Erbium 167.26");
+            break;
+        case 69:
+            printf("Tm Thulium 168.93");
+            break;
+        case 70:
+            printf("Yb Ytterbium 173.05");
+            break;
+        case 71:
+            printf("Lu Lutetium 174.97");
+            break;
+        case 72:
+            printf("Hf Hafnium 178.49");
+            break;
+        case 73:
+            printf("Ta Tantalum 180.95");
+            break;
+        case 74:
+            printf("W Tungsten 183.84");
+            break;
+        case 75:
+            printf("Re Rhenium 186.21");
+            break;
+        case 76:
+            printf("Os Osmium 190.23");
+            break;
+        case 77:
+            printf("Ir Iridium 192.22");
+            break;
+        case 78:
+            printf("Pt Platinum 195.08");
+            break;
+        case 79:
+            printf("Au Gold 196.97");
+            break;
+        case 80:
+            printf("Hg Mercury 200.59");
+            break;
+        case 81:
+            printf("Tl Thallium 204.38");
+            break;
+        case 82:
+            printf("Pb Lead 207.2");
+            break;
+        case 83:
+            printf("Bi Bismuth 208.98");
+            break;
+        case 84:
+            printf("Po Polonium 209");
+            break;
+        case 85:
+            printf("At Astatine 210");
+            break;
+        case 86:
+            printf("Rn Radon 222");
+            break;
+        case 87:
+            printf("Fr Francium 223");
+            break;
+        case 88:
+            printf("Ra Radium 226");
+            break;
+        case 89:
+            printf("Ac Actinium 227");
+            break;
+        case 90:
+            printf("Th Thorium 232.04");
+            break;
+        case 91:
+            printf("Pa Protactinium 231.04");
+            break;
+        case 92:
+            printf("U Uranium 238.03");
+            break;
+        case 93:
+            printf("Np Neptunium 237");
+            break;
+        case 94:
+            printf("Pu Plutonium 244");
+            break;
+        case 95:
+            printf("Am Americium 243");
+            break;
+        case 96:
+            printf("Cm Curium 247");
+            break;
+        case 97:
+            printf("Bk Berkelium 247");
+            break;
+        case 98:
+            printf("Cf Californium 251");
+            break;
+        case 99:
+            printf("Es Einsteinium 252");
+            break;
+        case 100:
+            printf("Fm Fermium 257");
+            break;
+        case 101:
+            printf("Md Mendelevium 258");
+            break;
+        case 102:
+            printf("No Nobelium 259");
+            break;
+        case 103:
+            printf("Lr Lawrencium 262");
+            break;
+        case 104:
+            printf("Rf Rutherfordium 267");
+            break;
+        case 105:
+            printf("Db Dubnium 270");
+            break;
+        case 106:
+            printf("Sg Seaborgium 271");
+            break;
+        case 107:
+            printf("Bh Bohrium 270");
+            break;
+        case 108:
+            printf("Hs Hassium 277");
+            break;
+        case 109:
+            printf("Mt Meitnerium 276");
+            break;
+        case 110:
+            printf("Ds Darmstadtium 281");
+            break;
+        case 111:
+            printf("Rg Roentgenium 280");
+            break;
+        case 112:
+            printf("Cn Copernicium 285");
+            break;
+        case 113:
+            printf("Nh Nihonium 284");
+            break;
+        case 114:
+            printf("Fl Flerovium 289");
+            break;
+        case 115:
+            printf("Mc Moscovium 288");
+            break;
+        case 116:
+            printf("Lv Livermorium 293");
+            break;
+        case 117:
+            printf("Ts Tennessine 294");
+            break;
+        case 118:
+            printf("Og Oganesson 294");
+            break;
+        default:
+            printf("Element not found\n");
+            break;
+        }
+        printf("\n*********************************\n");
+    } 
+}
